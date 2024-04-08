c19a3757ed4f148bf016332d4e344d8c7739e1b5
erustus07 <erustusndegwa08@gmail.com>
2024-04-08 12:20:05 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userInfo = {
+      name: userName,
+      email: userEmail
+    }
+  
+    const configUser = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    }
+  
+    return fetch('http://localhost:3000/users', configUser)
+      .then(response => response.json())
+      .then(data => document.body.innerHTML = data.id)
+      .catch(error => document.body.innerHTML = error.message)
+  }
